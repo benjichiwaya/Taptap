3ebb157ea320381c3ae8cbc047bf1ef76175cd68
benjichiwaya <benjaminmuyinda@gmail.com>
2022-09-01 11:05:39 -0400

    GUI Update and bug fixes

--- app/src/main/java/benji/chiwaya/taptap/MainActivity.java
+++ app/src/main/java/benji/chiwaya/taptap/MainActivity.java
@@ -3,7 +3,10 @@ package benji.chiwaya.taptap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,16 +19,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import static android.view.View.VISIBLE;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class MainActivity extends AppCompatActivity {
     
     private static final String TAG = "TapTap App";
     private AtomicBoolean START = new AtomicBoolean(false);
     private int CHANCE = 1;
     private int SCORE = 0;
-    private int SPEED = 2000;
+    private int HIGHSCORE = 10;
+    private int SPEED = 3000;
     private String[] colors_array;
     private int[] backgrounds_array;
     private int Level_up = 50;
+    private SharedPreferences sp_database;
     private TextView colors_text_view, high_score, new_score, game_over;
     private Button gameButton;
     private Dialog dialog;
@@ -36,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         colors_text_view = findViewById(R.id.text);
-        
+
+        sp_database = getApplicationContext()
+                .getSharedPreferences("Taptap", Context.MODE_PRIVATE);
+
         dialog = new Dialog(this);
         
         dialog.setContentView(R.layout.new_game);
@@ -44,16 +55,16 @@ public class MainActivity extends AppCompatActivity {
         high_score = dialog.findViewById(R.id.highscore);
         new_score = dialog.findViewById(R.id.newscore);
         game_over = dialog.findViewById(R.id.gameOver);
+
         
         colors_array = getResources().getStringArray(R.array.colors_array);
         backgrounds_array = new int[]{Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN, Color.MAGENTA,};
         
         dialog.show();
         
-            t = new Thread() {
+        t = new Thread() {
                 @Override
-                public void run() {
-                    try {
+                public void run() {try {
                         while (!isInterrupted()) {
                             
                             Thread.sleep(getSPEED());
@@ -63,14 +74,13 @@ public class MainActivity extends AppCompatActivity {
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }
-                }
+                    }}
             };
-            t.start();
-    
+        t.start();
         gameButton.setOnClickListener(view -> {
             reset();
         });
+
     }
     
     
@@ -84,98 +94,98 @@ public class MainActivity extends AppCompatActivity {
         t.setText(colors_array[color]);
         
         int background = random.nextInt(backgrounds_array.length);
-        t.setTextColor(backgrounds_array[background]);
+        findViewById(R.id.mainlayout).setBackgroundColor(backgrounds_array[background]);
     }
     
     protected boolean Verify(TextView t) {
-        int color =t.getCurrentTextColor();
-        
+
+        ColorDrawable viewColor = (ColorDrawable) (findViewById(R.id.mainlayout).getBackground());
+        int color = viewColor.getColor();
+
         switch (color)
         {
-            case Color.RED:    if(t.getText().toString() == colors_array[0])
-            {
-                Toast.makeText(getApplicationContext(),"POINT",Toast.LENGTH_SHORT).show();
+            case Color.RED:     if(t.getText().toString() == colors_array[0])
+                {
                 return true;
-            }
-            else{
-                return false;}
-            
-            case Color.GREEN:  if(t.getText().toString() == colors_array[3])
-            {
-                Toast.makeText(getApplicationContext(),"POINT",Toast.LENGTH_SHORT).show();
+                }
+            case Color.GREEN:   if(t.getText().toString() == colors_array[3])
+                {
                 return true;
-            }
-            else{
-                return false;}
-            
-            case Color.BLUE:   if(t.getText().toString() == colors_array[2])
-            {
+                }
+            case Color.BLUE:    if(t.getText().toString() == colors_array[2])
+                 {
                 return true;
-            }
-            else{
-                return false;}
-            
-            case Color.YELLOW: if(t.getText().toString() == colors_array[1])
-            {
-                Toast.makeText(getApplicationContext(),"POINT",Toast.LENGTH_SHORT).show();
+                 }
+            case Color.YELLOW:  if(t.getText().toString() == colors_array[1])
+                {
                 return  true;
-            }
-            else{
-                return false;}
-            
+                }
             case Color.MAGENTA:if(t.getText().toString() == colors_array[4])
-            {
-                Toast.makeText(getApplicationContext(),"POINT", Toast.LENGTH_SHORT).show();
+                {
                 return true;
-            }
-            else{
-                return false;}
-            
+                }
+
             default:
                 return false;
         }
     }
     
     public void tvTap(View view) {
-        
         if(Verify(colors_text_view)){
-            
+            YoYo.with(Techniques.BounceIn)
+                    .duration(100)
+                    .playOn(colors_text_view);
             SCORE+=10;
             Log.e(TAG,String.valueOf(SCORE));
             rotate_colors(colors_text_view);
             
             if(SCORE == Level_up)
             {
-                setSPEED(SPEED-=500);
-                Level_up+=30;
+                setSPEED(SPEED=SPEED-250);
+                Level_up=Level_up+30;
             }
         }
-        else {
-            CHANCE--;
+        else if(CHANCE == 0){
+            SharedPreferences.Editor save_highscore = sp_database.edit();
+
+            if((sp_database.getInt("highscore",0) == 0 )|| (sp_database.getInt("highscore", 0)) <= SCORE)
+            {
+                save_highscore.putInt("highscore",SCORE);
+                save_highscore.commit();
+                HIGHSCORE = SCORE;
             }
-        if(CHANCE == 0)
-        {
+
+            HIGHSCORE = sp_database.getInt("highscore",0);
+
             dialog.show();
-            high_score.setText(getString(R.string.high_score)+String.valueOf(SCORE*10));
-            new_score.setText(getString(R.string.new_score)+String.valueOf(SCORE));
+            high_score.setText(getString(R.string.high_score)+(HIGHSCORE));
+            new_score.setText(getString(R.string.new_score)+(SCORE));
             game_over.setVisibility(VISIBLE);
+        }
+        else {
+            YoYo.with(Techniques.Tada)
+                    .duration(100)
+                    .playOn(colors_text_view);
+
+            CHANCE--;
         }
     }
     
     private void reset()
     {
+        SharedPreferences.Editor save_highscore = sp_database.edit();
+        HIGHSCORE = sp_database.getInt("highscore",0);
         START.set(true);
         CHANCE = 4;
         SCORE = 10;
         SPEED = 3000;
         dialog.dismiss();
+
     }
-    
-    
-    public int getSPEED() {
+    public int getSPEED()
+    {
         return SPEED;
     }
-    
     public void setSPEED(int SPEED) {
         this.SPEED = SPEED;
     }
